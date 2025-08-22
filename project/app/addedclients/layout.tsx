a6cfdb0016e67990378e06d3af1776e0337c1a00
Kyrie <your-email@example.com>
2025-08-22 21:11:21 +0800

    Commit

--- project/app/addedclients/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import { useState } from "react"
-
-export default function AddedClientsLayout({ children }) {
-  const [activeTab, setActiveTab] = useState("addedclients")
-
-  return <>{children}</>
-}
